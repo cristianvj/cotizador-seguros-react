f49b2bbd1c7590089dfd0895fb8e5f4ad829891f
Cristian David <cristiandavid.vj@gmail.com>
2020-01-20 18:25:12 -0500

    Formulario

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+"Cotizador de seguros con React Hooks"
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: auto;
+`;
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <Contenedor>
+      <Header 
+        titulo="Cotizador de Seguros" 
+      />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
